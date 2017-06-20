60e94297b32470eef28d6ff95a6f522315a5a910
cacdcentral <gustavo.chavez@ca.com>
2017-06-20 10:51:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<HEAD>
+ <script type="text/javascript" id="ca_eum_ba" agent=browser src="https://cloud.ca.com/mdo/v1/sdks/browser/BA.js" data-profileUrl="https://collector-axa.cloud.ca.com/api/1/urn:ca:tenantId:CACDNA01-USERSTORE/urn:ca:appId:MSF%20-%20NACentral/profile?agent=browser" data-tenantID="CACDNA01-USERSTORE" data-appID="MSF - NACentral" data-appKey="afcefef0-55cb-11e7-b4bd-09d934e182f7" ></script> 
+<HEAD>
+
 <?php
 require_once('PageRunner.php');
 
