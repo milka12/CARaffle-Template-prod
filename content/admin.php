23cffea912e84126ec48ccbb7798ce26f7ef2a35
cacdcentral <gustavo.chavez@ca.com>
2017-06-20 10:51:37 -0500

    Update admin.php

--- content/admin.php
+++ content/admin.php
@@ -2,7 +2,9 @@
 $dataFileName = getcwd() . DIRECTORY_SEPARATOR . 'mydata.txt';
 $count = file_exists($dataFileName) ? COUNT(FILE($dataFileName)) : 0;
 ?>
-
+<HEAD>
+ <script type="text/javascript" id="ca_eum_ba" agent=browser src="https://cloud.ca.com/mdo/v1/sdks/browser/BA.js" data-profileUrl="https://collector-axa.cloud.ca.com/api/1/urn:ca:tenantId:CACDNA01-USERSTORE/urn:ca:appId:MSF%20-%20NACentral/profile?agent=browser" data-tenantID="CACDNA01-USERSTORE" data-appID="MSF - NACentral" data-appKey="afcefef0-55cb-11e7-b4bd-09d934e182f7" ></script> 
+<HEAD>
 <style scoped>
     content {
         height: 100%;
