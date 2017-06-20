56354ad6531ac31637ca94717f2d4c979de5e211
cacdcentral <gustavo.chavez@ca.com>
2017-06-20 10:52:10 -0500

    Update welcome.php

--- content/welcome.php
+++ content/welcome.php
@@ -27,7 +27,9 @@ if(isset($_POST[$nameInput]) && !empty($_POST[$nameInput])) {
     $message = 'The field cannot be empty.<br/>Please click the home button to return to the entry form and re-enter your name.';
 }
 ?>
-
+<HEAD>
+ <script type="text/javascript" id="ca_eum_ba" agent=browser src="https://cloud.ca.com/mdo/v1/sdks/browser/BA.js" data-profileUrl="https://collector-axa.cloud.ca.com/api/1/urn:ca:tenantId:CACDNA01-USERSTORE/urn:ca:appId:MSF%20-%20NACentral/profile?agent=browser" data-tenantID="CACDNA01-USERSTORE" data-appID="MSF - NACentral" data-appKey="afcefef0-55cb-11e7-b4bd-09d934e182f7" ></script> 
+<HEAD>
 <style scoped>
     .stop {
         width: 200px;
